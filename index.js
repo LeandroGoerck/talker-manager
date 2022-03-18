30680d629c27322293f89ca6da177500fa4b70a7
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-03-18 17:46:36 -0300

    bugfix fixed task 1 getAllTalkers

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const seed = require('./tests/seed.json');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const FILE_NAME = './talker.json';
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,7 +17,13 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', (request, response) => {
-  response.status(200).json(seed);
+  fs.readFile(FILE_NAME, 'utf8')
+  .then((JSONstring) => JSON.parse(JSONstring))
+  .then((data) => response.status(200).json(data))
+  .catch((err) => {
+    console.error(`Error reading file ${FILE_NAME}\n Error: ${err}`);
+    process.exit(1);
+  });
 });
 
 app.listen(PORT, () => {
